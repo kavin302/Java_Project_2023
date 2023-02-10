0b1c1cdde4852f0d53ac6f21a7088cbcf638f417
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-02-10 08:32:37 +0530

    Day02

---
+++ src/Day02/Program02.java
@@ -0,0 +1,17 @@
+package Day02;
+
+public class Program02 {
+
+	public static int fun()
+	{
+		int x=10;
+		System.out.println(x*x);
+		return x*x;
+	}
+
+	
+	public static void main(String[] args) {
+		long a=fun();//64,32//100.0
+		System.out.println(a+a);//200.0
+	}
+}
