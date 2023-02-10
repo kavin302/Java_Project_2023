5165eb25e2e4e4518ae6be62f42357442e966e37
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-02-10 08:44:51 +0530

    Day03

---
+++ src/Day03Test01/Program01.java
@@ -0,0 +1,23 @@
+package Day03Test01;
+
+public class Program01 {
+
+	
+		public void addMethod(int a,int b)
+		{
+			System.out.println(a+b);
+		}
+		
+		public static void subMethod(int a,int b)
+		{
+			System.out.println(a-b);
+		}
+		
+		public static void mulMethod(int a,int b)
+		{
+			System.out.println(a*b);
+		}
+
+	}
+
+
