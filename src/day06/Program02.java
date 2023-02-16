bad5080dd7fb1f83c228ddf74cd67824ec6dc42a
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-02-16 11:08:17 +0530

    Day06

---
+++ src/day06/Program02.java
@@ -0,0 +1,27 @@
+package day06;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		double c=20.0;
+		System.out.println("%%%%%%%%%%%%%%");
+		System.out.println(a>b);//f
+		System.out.println(b>a);//t
+		System.out.println(a<b);//t
+		System.out.println(b<a);//f
+		System.out.println("%%%%%%%%%%%%%%");
+		System.out.println(a>=b);//f
+		System.out.println(b>=a);//t
+		System.out.println(a<=b);//t
+		System.out.println(b<=c);//t		
+		System.out.println("%%%%%%%%%%%%%%");
+		System.out.println(a==b);//f
+		System.out.println(b!=a);//t
+		System.out.println(b==c);//t
+		System.out.println(b!=c);//f
+	}
+
+}
