9be6780f6fadfbca990d28a55db6cd083dc03bb7
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-02-16 11:04:50 +0530

    Day05

---
+++ src/day05/test02/Program02.java
@@ -0,0 +1,14 @@
+package day05.test02;
+public class Program02 extends Program01{
+	
+	public static void display2()
+	{
+		System.out.println("Good Day..!");
+	}
+	
+	public void display2NonStatic()
+	{
+		System.out.println("Good Day..!");
+	}
+
+}
