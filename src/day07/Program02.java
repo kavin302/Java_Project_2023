b7f5007060d56cc016082156b667e30c138e0fa3
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-02-16 11:16:14 +0530

    Day07

---
+++ src/day07/Program02.java
@@ -0,0 +1,25 @@
+package day07;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100;
+		int b=20;
+
+		System.out.println("Start Program...!");
+
+		if(a>b)
+		{
+			System.out.println("a is greater..!");
+		}
+		else
+		{
+			System.out.println("b is greater..!");
+		}
+
+		System.out.println("End Program...!");
+
+	}
+
+}
