e4e608b5baf8336c5b3f6dd68e0f25795699b0ca
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-02-17 08:22:46 +0530

    Day10

---
+++ src/day10/Program01.java
@@ -0,0 +1,25 @@
+package day10;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1;
+		do
+		{
+			System.out.println("Good Day..!");
+			a=a+1;
+		}while(a<=5);
+		System.out.println("$$$$$$$$$$$$$$$$$$$");
+
+		int b=5;
+		do
+		{
+			System.out.println("Good Day..!");
+			b=b-1;
+		}while(b>=1);
+
+
+	}
+
+}
