9c03e602b9a2b7dac7c5cc5651503ab799c4551e
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-02-17 08:29:50 +0530

    Day10

---
+++ src/day10/Program02.java
@@ -0,0 +1,13 @@
+package day10;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          for(int a=1;a<=5;a++)
+          {
+        	  System.out.println("Good day...!");  
+          }
+	}
+
+}
