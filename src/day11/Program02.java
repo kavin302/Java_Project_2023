fd92c975b21e68465bb06f1ecb3d528f5054487a
kavin302 <123706517+kavin302@users.noreply.github.com>
2023-02-24 08:35:30 +0530

    Day11

---
+++ src/day11/Program02.java
@@ -0,0 +1,35 @@
+package day11;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=1;i++)
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+		for(int i=1;i<=2;i++)
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+		for(int i=1;i<=3;i++)
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+		for(int i=1;i<=4;i++)
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print(i);
+		}
+		System.out.println();
+
+	}
+
+}
